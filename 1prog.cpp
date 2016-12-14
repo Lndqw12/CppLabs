49ca3ab0cc7db67637e57aa7a8e1027afe2d906a
Lndqw12 <COMP222012016@mail.ru>
2016-12-14 17:41:20 +0300

    cpp7lab

--- 1prog.cpp
+++ 1prog.cpp
@@ -2,16 +2,19 @@
 using namespace std;
 int myfunc (int a, int b){
     int sum;
-    sum = a+b;
+    sum = a*b;
     return sum;
 }
 
 int main () {
     int k,m,sum;
+    string stroka("text");
+    cin>>stroka;
     cin>>k;
     cin>>m;
     sum = myfunc(k,m);
-    cout<<sum;
+    cout<<sum<<endl;
+    cout<<stroka<<endl;
     system("pause");
     return 0;   
 }
