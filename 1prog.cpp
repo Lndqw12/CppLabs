96f42b7b9cfdb693a125372c23b7bb6cb1fb5da9
Lndqw12 <COMP222012016@mail.ru>
2016-12-14 16:45:11 +0300

    cpp2,3lab

--- 1prog.cpp
+++ 1prog.cpp
@@ -3,6 +3,13 @@ using namespace std;
 int main() {
     cout << "Hello, world!\n";
     
+    int a,b,sum;
+    cout << "WRITE 'A'\n";
+    cin >> a;
+    cout << "WRITE 'B'\n";
+    cin >> b;
+    sum=a * b;
+    cout<<sum<<endl;
     system("pause");
     return 0;
 }
