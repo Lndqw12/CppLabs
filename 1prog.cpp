14c54bb5cdc48ebffd5d796ea8b2b4160c5189bd
Lndqw12 <COMP222012016@mail.ru>
2016-12-14 17:03:09 +0300

    cpp4lab

--- 1prog.cpp
+++ 1prog.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    cout << "Hello, world!\n";
-    
-    int a,b,sum;
-    cout << "WRITE 'A'\n";
-    cin >> a;
-    cout << "WRITE 'B'\n";
-    cin >> b;
-    sum=a * b;
-    cout<<sum<<endl;
+    string text;
+    cout << "WRITE TEXT\n";
+    cin >> text;
+    cout <<text<<endl;
     system("pause");
     return 0;
 }
+
